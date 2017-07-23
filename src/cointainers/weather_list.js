82dcc813efd5b1109c6c92c7994205bc5eb5dc80
csuvikg <gaborcsuvik@gmail.com>
2017-07-23 19:02:17 +0200

    Added GoogleMaps display

--- src/cointainers/weather_list.js
+++ src/cointainers/weather_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 export class WeatherList extends Component {
     renderWeather(cityData) {
@@ -8,12 +9,13 @@ export class WeatherList extends Component {
         const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 272.15);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
 
         console.log(temps);
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/>p</td>
                 <td><Chart data={temps} color="orange" units="°C"/></td>
                 <td><Chart data={pressures} color="green" units="hPa"/></td>
                 <td><Chart data={humidities} color="black" units="%"/></td>
@@ -25,15 +27,15 @@ export class WeatherList extends Component {
         return (
             <table className="table table-hover">
                 <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature (°C)</th>
-                        <th>Pressure (hPa)</th>
-                        <th>Humidity (%)</th>
-                    </tr>
+                <tr>
+                    <th>City</th>
+                    <th>Temperature (°C)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {this.props.weather.map(this.renderWeather)}
+                {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
